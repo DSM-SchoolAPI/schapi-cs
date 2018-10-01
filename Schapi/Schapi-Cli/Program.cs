2963a9435a42d1605337ceb601109c0acd9df686
XxshiftxX <qjfrntop12@naver.com>
2018-10-01 19:45:39 +0900

    Fix split bug in menu with slash

--- Schapi/Schapi-Cli/Program.cs
+++ Schapi/Schapi-Cli/Program.cs
@@ -19,7 +19,7 @@ namespace Schapi_Cli
                 "G100000170"
             );
 
-            Console.WriteLine(api.GetMonthlyMenus(2018, 10)[1].BreakfastString);
+            Console.WriteLine(api.GetMonthlyMenus(2018, 10)[3].BreakfastString);
             foreach (var menu in api.GetMonthlyMenus(2018, 10)[23].Lunch)
             {
                 Console.WriteLine(menu);
